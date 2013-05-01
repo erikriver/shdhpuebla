4b2789017bb89d433c0d012439c64733ddaf1a31
Erik Rivera <erik.river@gmail.com>
2013-04-30 21:42:35 -0500

    nuevo metodo de autenticacion

--- shdhpuebla/__init__.py
+++ shdhpuebla/__init__.py
@@ -13,13 +13,28 @@ def main(global_config, **settings):
     
     session_factory = UnencryptedCookieSessionFactoryConfig( settings['cookie.secret'], )
     
+    providers = settings.get('login_providers', '')
+    providers = filter(None, [p.strip()
+                       for line in providers.splitlines()
+                       for p in line.split(', ')])
+    settings['login_providers'] = providers
+    
     config = Configurator(settings=settings, session_factory=session_factory )
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('add', '/register')
     
-    config.add_route("facebook_login", "/facebook/login")
-    config.add_route("twitter_login", "/twitter/login")
+    if 'facebook' in providers:
+        config.include('velruse.providers.facebook')
+        config.add_facebook_login_from_settings(prefix='facebook.')
+    
+    if 'twitter' in providers:
+        config.include('velruse.providers.twitter')
+        config.add_twitter_login_from_settings(prefix='twitter.')
+    
+    config.add_route('home', '/')
+    config.add_route('register', 'register')
+    
+#    config.add_route("facebook_login", "/facebook/login")
+#    config.add_route("twitter_login", "/twitter/login")
     
     config.scan()
     return config.make_wsgi_app()
--- shdhpuebla/views.py
+++ shdhpuebla/views.py
@@ -1,49 +1,85 @@
-import cgi
 import json
 import urllib
-import urlparse
 
 from pyramid.view import view_config
 from pyramid.url import route_url
 from pyramid.httpexceptions import HTTPFound
 
-import oauth2 as oauth
+from velruse import login_url
 
 from .models import DBSession, Attendance, Event
-
-TWITTER_REQUEST_TOKEN_URL = 'http://twitter.com/oauth/request_token'
-TWITTER_ACCESS_TOKEN_URL = 'http://twitter.com/oauth/access_token'
-TWITTER_AUTHORIZE_URL = 'http://twitter.com/oauth/authorize'
 
 
 @view_config(route_name='home', renderer='templates/index.pt')
 def index(request):
     event = DBSession.query(Event).filter(Event.active==True).first()
-    return {'event':event}
+    return {
+            'event':event,
+            'login_url': login_url,
+            'providers': request.registry.settings['login_providers'],
+    }
 
-@view_config(route_name='add', renderer='templates/register.pt')    
-def register(request):
-    session = request.session
-    db_session = DBSession()
+@view_config(
+    context='velruse.AuthenticationComplete',
+#    route_name='register', 
+    renderer='templates/register.pt')    
+def login(request):
     
-    if 'facebook' in session:
-        network = 'facebook'
-    if 'twitter' in session:
-        network = 'twitter'
+    context = request.context
+    profile = context.profile
+    network = context.provider_type
+    data = {}
     
-    data = session[network]
+    if network == 'twitter':
+        data['user_id'] = profile.get('displayName','')
+        
+        user_data = json.loads(urllib.urlopen(
+                    'http://api.twitter.com/1/users/show.json?%s' %
+                    urllib.urlencode(dict(screen_name=data['user_id']))).read())
+        
+        data['user_name'] = user_data.get('name','')
+        data['url'] = 'https://twitter.com/#!/'+data['user_id'],
+        data['image_url'] = user_data.get('profile_image_url',''),
+        data['email'] = ''
+        
+    if network == 'facebook':
+        data['user_id'] = profile['accounts'][0]['userid']
+        data['user_name'] = profile.get('displayName','')
+        data['email'] = profile['emails'][0]['value']
+        data['image_url'] = 'https://graph.facebook.com/'+data['user_id']+'/picture'
+        data['url'] = 'http://facebook.com/'+data['user_id']
     
+    db_session = DBSession()    
     event = db_session.query(Event).filter(Event.active==True).first()
     attende = DBSession.query(Attendance).filter(Attendance.user_id==data['user_id']).first()
+            
+    tasks = ''
+    if attende:
+        tasks = attende.tasks
+        data['email'] = attende.email
     
+    data['tasks'] = tasks
+    data['network'] = network
+    
+    return data
+
+@view_config(route_name='register')
+def register(request):
     if 'submit' in request.POST:
-        
-        data['network'] = network
-        data['event'] = event
+        db_session = DBSession()
+        data = {}
+        data['network'] = request.params['network']
+        data['user_id'] = request.params['user_id']
         data['user_name'] = request.params['name']
         data['email'] = request.params['email']
         data['tasks'] = request.params['message']
+        data['image_url'] = request.params['image_url']
+        data['url'] = request.params['url']
         
+        event = db_session.query(Event).filter(Event.active==True).first()
+        attende = DBSession.query(Attendance).filter(Attendance.user_id==data['user_id']).first()
+        data['event'] = event
+                
         if attende:
             attende.user_name = data['user_name']
             attende.email = data['email']
@@ -56,125 +92,10 @@ def register(request):
         db_session.add(attende)
             
         return HTTPFound(location = request.route_url('home'))    
-        
-    tasks = ''
-    if attende:
-        tasks = attende.tasks
-        data['email'] = attende.email
-    
-    user_name = data['user_name']
-    email = data['email']
-    
-    return {'user_name':user_name, 'email':email, 'tasks':tasks}
-
-@view_config(route_name='facebook_login')    
-def facebook_login_view(request):
-    config = request.registry.settings
-    FACEBOOK_SCOPE = config.get('facebook.scope')
-    FACEBOOK_APP_ID = config.get('facebook.app_id')
-    FACEBOOK_APP_SECRET = config.get('facebook.app_secret')
-    
-    referer = request.params.get('referer', route_url('add', request))
-    if referer.startswith(route_url('facebook_login', request)):
-        referer = route_url('add', request)
-        
-    args = dict(client_id=FACEBOOK_APP_ID, scope=FACEBOOK_SCOPE,
-            redirect_uri=route_url('facebook_login', request,
-                                   _query=[('referer', referer)]))
-    verification_code = request.params.get('code', None)
-    if not verification_code:
-        return HTTPFound(location='https://graph.facebook.com/oauth/authorize?%s'
-                         % urllib.urlencode(args))
-
-    args['client_secret'] = FACEBOOK_APP_SECRET
-    args['code'] = request.params.get('code', '')
-    access_token_url = ('https://graph.facebook.com/oauth/access_token?%s' %
-                        urllib.urlencode(args))
-    response = cgi.parse_qs(urllib.urlopen(access_token_url).read())
-    access_token = response['access_token'][-1]
-    session = request.session
-    if not access_token:
-        session.flash(u'Facebook login error', queue='flash_error')
-        session['facebook'] = None
-        return HTTPFound(location=route_url('home', request))
-    facebook_user = json.loads(urllib.urlopen(
-            'https://graph.facebook.com/me?%s' %
-            urllib.urlencode(dict(access_token=access_token))).read())
-    session['facebook'] = { 'token':access_token,
-                            'token_secret':'',
-                            'user_id':facebook_user.get(u'id', u''),
-                            'user_name':facebook_user.get(u'name', u''),
-                            'url':facebook_user.get(u'link', u''),
-                            'email':facebook_user.get(u'email', u''),
-                            'image_url':'https://graph.facebook.com/'+facebook_user.get(u'id')+'/picture',}
-                            
-    return HTTPFound(location=referer)
-    
 
 
-@view_config(route_name='twitter_login')
-def twitter_login_view(request):
-    config = request.registry.settings
-    TWITTER_CONSUMER_SECRET = config.get('twitter.consumer_secret')
-    TWITTER_CONSUMER_KEY    = config.get('twitter.consumer_key')
-    consumer = oauth.Consumer(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-    
-    referer = request.params.get('referer', route_url('add', request))
-    
-    if referer.startswith(route_url('twitter_login', request)):
-        referer = route_url('add', request)
-
-    session = request.session
-    if session.get('twitter_access_token', u'') and session.get(
-            'twitter_access_token_secret', u''):
-        return HTTPFound(location=referer)
-
-    twitter_request_token = session.get('twitter_request_token', u'')
-    session['twitter'] = None
-    if not twitter_request_token:
-        twitter_callback_uri = route_url('twitter_login', request,
-                _query=[('referer', referer)])
-        client = oauth.Client(consumer)
-        body = urllib.urlencode(dict(oauth_callback=twitter_callback_uri))
-        resp, content = client.request(TWITTER_REQUEST_TOKEN_URL, 'POST',
-                body=body)
-        if resp['status'] != '200':
-            session.flash(u'Twitter login error', queue='flash_error')
-            session['twitter_request_token'] = None
-            return HTTPFound(location=route_url('home', request))
-        else:
-            twitter_request_token = dict(urlparse.parse_qsl(content))
-            session['twitter_request_token'] = twitter_request_token
-            return HTTPFound(location='%s?oauth_token=%s' % (TWITTER_AUTHORIZE_URL,
-                    twitter_request_token['oauth_token']))
-
-    # redirect back from Twitter
-    token = oauth.Token(twitter_request_token['oauth_token'],
-                        twitter_request_token['oauth_token_secret'])
-    twitter_oauth_verifier = request.params.get('oauth_verifier', '')
-    if not twitter_oauth_verifier:
-        session.flash(u'Twitter login error', queue='flash_error')
-        session['twitter'] = None
-        return HTTPFound(location=route_url('home', request))
-
-    token.set_verifier(twitter_oauth_verifier)
-    client = oauth.Client(consumer, token)
-    resp, content = client.request(TWITTER_ACCESS_TOKEN_URL, "POST")
-    access_token = dict(urlparse.parse_qsl(content))
-    
-    twitter_user = json.loads(urllib.urlopen(
-            'http://api.twitter.com/1/users/show.json?%s' %
-            urllib.urlencode(dict(screen_name=access_token['screen_name']))).read())
-        
-    session['twitter'] = {  'token': access_token['oauth_token'],
-                            'token_secret': access_token['oauth_token_secret'],
-                            'user_id': access_token['screen_name'],
-                            'user_name': twitter_user.get('name',''),
-                            'url': 'https://twitter.com/#!/'+access_token['screen_name'],
-                            'email': '',
-                            'image_url': twitter_user.get('profile_image_url',''),
-                        }
-    
-    
-    return HTTPFound(location=referer)
-    
+@view_config(context='velruse.AuthenticationDenied')
+def login_denied_view(request):
+    return {
+        'result': 'denied',
+    }
